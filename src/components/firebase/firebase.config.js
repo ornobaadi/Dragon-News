927139c86de517491da4adaf3f866c57ea9f5844
ornobaadi <aadi4789@gmail.com>
2024-11-24 03:28:22 +0600

    Firebase secure

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA5jXPVe2rJ5BWSNmFMbUihHHpHVasbLro",
-    authDomain: "dragon-news-a450e.firebaseapp.com",
-    projectId: "dragon-news-a450e",
-    storageBucket: "dragon-news-a450e.firebasestorage.app",
-    messagingSenderId: "763440007901",
-    appId: "1:763440007901:web:6a5bd85c29879c700ca934"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
